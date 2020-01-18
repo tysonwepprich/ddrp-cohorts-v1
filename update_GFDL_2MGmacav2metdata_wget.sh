ab42f7841bd3fe2a72ae3f7eb8cc4c37c2e03b3f
tysonwepprich <tyson.wepprich@gmail.com>
2020-01-17 17:50:33 -0800

    new bash script to download more maca

---
+++ update_GFDL_2MGmacav2metdata_wget.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2021_2025_CONUS_daily.nc" -O macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2021_2025_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2031_2035_CONUS_daily.nc" -O macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2031_2035_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2041_2045_CONUS_daily.nc" -O macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2041_2045_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2051_2055_CONUS_daily.nc" -O macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2051_2055_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2061_2065_CONUS_daily.nc" -O macav2metdata_tasmin_GFDL-ESM2M_r1i1p1_rcp85_2061_2065_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2021_2025_CONUS_daily.nc" -O macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2021_2025_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2031_2035_CONUS_daily.nc" -O macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2031_2035_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2041_2045_CONUS_daily.nc" -O macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2041_2045_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2051_2055_CONUS_daily.nc" -O macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2051_2055_CONUS_daily.nc  
+wget -nc -c -nd "http://thredds.northwestknowledge.net:8080/thredds/fileServer/MACAV2/GFDL-ESM2M/macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2061_2065_CONUS_daily.nc" -O macav2metdata_tasmax_GFDL-ESM2M_r1i1p1_rcp85_2061_2065_CONUS_daily.nc  
+
+
