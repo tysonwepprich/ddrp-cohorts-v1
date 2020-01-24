c2769f8f681a0c5839529640d7e3e2630abf6e9b
tysonwepprich <tyson.wepprich@gmail.com>
2020-01-24 13:38:27 -0800

    parallel MACA raster split

--- MACA_dailyraster.R
+++ MACA_dailyraster.R
@@ -5,6 +5,9 @@
 library(raster)
 library(ncdf4)
 library(stringr)
+library(foreach)
+library(doParallel)
+library(parallel)
 
 # longitude in degrees EAST
 # time is days since 1900-01-01 00:00:00
@@ -14,26 +17,36 @@ library(stringr)
 
 
 # Function to split 5-year data into daily tmin/tmax rasters for use in lifecycle model
-SplitMACA <- function(macafile, targetdir){
+SplitMACA <- function(macafile, targetdir, ncores = 10){
+  cl <<- makePSOCKcluster(ncores) # export to global environment
+  # If run is being done on Hopper, need to specify the library for each worker
+  if (Sys.info()["nodename"] == "hopper.science.oregonstate.edu") {
+    clusterEvalQ(cl, .libPaths("/usr/local/lib64/R/library/"))
+  }
+  registerDoParallel(cl)
+  
   ras <- brick(macafile, 
                varname = "air_temperature", lvar = 3, level = 4)
   
-  # # if you have permission to write, you could make directories for each year here
-  # yrs <- gregexpr(pattern = "[0-9]{4}", text = macafile)
-  # yr1 <- as.numeric(substr(macafile, start = yrs[[1]][1], stop = yrs[[1]][1] + 3))
-  # yr2 <- as.numeric(substr(macafile, start = yrs[[1]][2], stop = yrs[[1]][2] + 3))
+  # if you have permission to write, you could make directories for each year here
+  yrs <- gregexpr(pattern = "[0-9]{4}", text = macafile)
+  yr1 <- as.numeric(substr(macafile, start = yrs[[1]][1], stop = yrs[[1]][1] + 3))
+  yr2 <- as.numeric(substr(macafile, start = yrs[[1]][2], stop = yrs[[1]][2] + 3))
   var <- sub(pattern = "as", replacement = "", x = stringr::str_split_fixed(macafile, pattern = "_", n = 3)[, 2], fixed = TRUE)
-  # 
-  # for (y in yr1:yr2){
-  #   dir.create(paste(targetdir, y, sep = "/"))
-  # }
+  
+  for (y in yr1:yr2){
+    if (dir.exists(paste(targetdir, y, sep = "/")) == FALSE){
+      dir.create(paste(targetdir, y, sep = "/"))
+    }
+  }
   
   fname <- stringr::str_split_fixed(macafile, pattern = "_", 9)
   varname <- paste(var, fname[,3], fname[,5], "4kmD1", sep = "_")
-
+  
+  indices <- 1:nlayers(ras)
   # loop every day and write new daily rasters
   # shift longitude values and Kelvin temperature scale for MACA to match other raster data
-  for (d in 1:nlayers(ras)){
+  foreach(d = indices, .packages = c('raster', "ncdf4", "stringr")) %dopar% {
     dras <- shift(ras[[d]], dx = -360) - 273.15
     date <- gsub(pattern = "[^0-9]", replacement = "", x = names(dras))
     yr <- substr(date, start = 1, stop = 4)
@@ -41,9 +54,13 @@ SplitMACA <- function(macafile, targetdir){
     
     writeRaster(x = dras, filename = paste(targetdir, yr, newname, sep = "/"), format = "raster", overwrite = TRUE)
   }
+  
+  
+  stopCluster(cl)
+  
 }
 
 # t <- system.time({
-  # SplitMACA(macafile, "/home/macav2metdata")
+# SplitMACA(macafile, "/home/macav2metdata")
 # })
 
